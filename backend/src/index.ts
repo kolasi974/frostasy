2dd4e831d2fe74efcbeaf2f5f8403c7794336c09
Jérémy Lebon <jeremylebon@MacBook-Air-de-Jeremy.local>
2025-02-01 18:44:08 +0400

    insert query

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,23 @@
 import Fastify from 'fastify';
-
 const server = Fastify({ logger: true });
+import { PrismaClient } from '@prisma/client'
 
 server.get('/', async (request, reply) => {
-  return { message: 'Hello world !' };
+  const prisma = new PrismaClient()
+  try {
+    const depot = await prisma.depot.create({
+      data: {
+        label: "Quai Plate",
+        adresse: "Quai Plate",
+        coordonneeY: 0.0,
+        coordonneeX: 0.0
+      },
+    })
+    await prisma.$disconnect()
+    return { data: depot };
+  } catch (error) {
+    await prisma.$disconnect()
+  }
 });
 
 server.listen({ port: 8000 }, (err, address) => {
